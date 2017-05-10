ac97188b7b5c7a9c4d76cb4dc54fb8c4738f1631
Wlainer Ribeiro <wlainer@gmail.com>
2017-05-09 22:12:52 -0300

    "video 10"

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,11 @@
 exports.homeController = (req, res) => {
   res.render('index');
 }
+
+
+
+exports.addStore = (req, res) => {
+  res.render('editStore', {
+    title:'Add Store'
+  });
+}
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -4,5 +4,6 @@ const storeController = require('../controllers/storeController');
 
 // Do work here
 router.get('/', storeController.homeController);
+router.get('/add', storeController.addStore);
 
 module.exports = router;
