ec4a63e48faff7559612b043d6dc1002d1b8119f
Wlainer Ribeiro <wlainer@gmail.com>
2017-05-11 13:33:54 -0300

    video 18

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next)  {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({message: 'That filetype isn\'nt allowed!'}, false);
+    }
+  }
+}
 
 exports.homeController = (req, res) => {
   res.render('index');
@@ -19,6 +34,22 @@ exports.createStore = async (req, res) => {
   res.redirect(`/store/${store.slug}`);
 }
 
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  if(!req.file) {
+    next();
+    return;
+  }
+
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
+};
 
 exports.getStores = async (req, res) => {
   const stores = await Store.find();
