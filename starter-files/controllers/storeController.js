ce4db9c0c11096d0acef1aa1d740317cb2755b51
Wlainer Ribeiro <wlainer@gmail.com>
2017-05-10 22:18:52 -0300

    video 14

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -27,3 +27,21 @@ exports.getStores = async (req, res) => {
     stores
   });
 }
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', {
+    title: `Edit ${store.name}`,
+    store
+  });
+}
+
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+    new: true,
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`) 
+  res.redirect(`/stores/${store._id}/edit`);
+}
