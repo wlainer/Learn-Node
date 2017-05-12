5cd3260a0f28f85aaf2b6e7c4578fe997979bebd
Wlainer Ribeiro <wlainer@gmail.com>
2017-05-12 20:26:03 -0300

    video 26

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -41,4 +41,7 @@ router.post('/register',
 
 router.get('/logout', authController.logout);
 
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
+
 module.exports = router;
