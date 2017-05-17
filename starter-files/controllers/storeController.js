654eb8534cb61873068bb3c27250baaf7a4201b0
Wlainer Ribeiro <wlainer@gmail.com>
2017-05-17 15:08:36 -0300

    video 29

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -29,6 +29,7 @@ exports.addStore = (req, res) => {
 
 
 exports.createStore = async (req, res) => {
+  req.body.author = req.user._id;
   const store = await (new Store(req.body)).save();  
   req.flash('success', `Successfully created. ${store.name}. Care to leave a review.`);
   res.redirect(`/store/${store.slug}`);
@@ -59,8 +60,15 @@ exports.getStores = async (req, res) => {
   });
 }
 
+const confirmOwner = (store, user) => {
+  if (!store.author.equals(user._id)) {
+    throw Error('You must own a store in order to edit it!')
+  }
+}
+
 exports.editStore = async (req, res) => {
   const store = await Store.findOne({ _id: req.params.id });
+  confirmOwner(store, req.user);
   res.render('editStore', {
     title: `Edit ${store.name}`,
     store
@@ -79,7 +87,7 @@ exports.updateStore = async (req, res) => {
 }
 
 exports.getStoreBySlug = async (req, res, next) => {
-  const store = await Store.findOne({slug: req.params['slug']});
+  const store = await Store.findOne({slug: req.params['slug']}).populate('author');
   if (!store) return next();
 
   res.render('store', {
