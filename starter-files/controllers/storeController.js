8bea75fa3220f8cc80f596b3efb06a052eebf9d1
Wlainer Ribeiro <wlainer@gmail.com>
2017-05-18 10:12:10 -0300

    video 31

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -110,3 +110,16 @@ exports.getStoresByTag = async (req, res) => {
     title: 'Tags'
   });
 }
+
+exports.searchStores = async (req, res) => {
+  const stores = await Store.find({
+    $text: {
+      $search: req.query.q
+    }
+  }, {
+    score: { $meta: 'textScore' }
+  }).sort({
+    score: {$meta: 'textScore'}
+  }).limit(5);
+  res.json(stores);
+}
